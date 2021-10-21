7fc5cf7f4edc1ac7e1fcdc8ffba53d9647b4b54d
DESKTOP-NDBQQLS\do1 <1497903789@qq.com>
2021-10-21 16:49:34 +0800

    feat: 增加新增文章页面，调整后台管理页面布局

--- src/views/manageSystem/components/markdown.vue
+++ src/views/manageSystem/components/markdown.vue
@@ -1,26 +1,98 @@
 <template>
-  <div>
-    <v-md-editor v-model="text" height="400px"></v-md-editor>
+  <div class="blog-wrap">
+    <h1>新增博客</h1>
+    <el-form
+      ref="blogForm"
+      :model="ruleForm"
+      :rules="rules"
+      label-width="120px"
+      class="demo-ruleForm"
+    >
+      <el-form-item label="博客标题" prop="title">
+        <el-input v-model="ruleForm.title"></el-input>
+      </el-form-item>
+      <el-form-item label="博客标签" prop="tag">
+        <el-checkbox-group v-model="ruleForm.tag">
+          <el-checkbox label="Javascript" name="tag"></el-checkbox>
+          <el-checkbox label="CSS" name="tag"></el-checkbox>
+          <el-checkbox label="HTML" name="tag"></el-checkbox>
+          <el-checkbox label="VUE" name="tag"></el-checkbox>
+        </el-checkbox-group>
+      </el-form-item>
+      <el-form-item label="博客描述" prop="desc">
+        <el-input v-model="ruleForm.desc" type="textarea"></el-input>
+      </el-form-item>
+      <el-form-item label="博客内容" prop="content">
+        <v-md-editor v-model="ruleForm.content" height="400px"></v-md-editor>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm('ruleForm')"
+          >新增</el-button
+        >
+        <el-button @click="resetForm('ruleForm')">重置</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
-<script lang='ts'>
-import { defineComponent, ref } from 'vue'
-export default defineComponent({
-  name: '',
-  props: {
-  },
-  components: {
-  },
-  setup () {
-    const mdValue = ref('')
-    return {
-      mdValue
-    }
-  }
-})
+<script>
+  import { defineComponent, ref, reactive, toRefs } from 'vue'
+  export default defineComponent({
+    name: '',
+    props: {},
+    components: {},
+    setup() {
+      const blogForm = ref(null)
+      const state = reactive({
+        ruleForm: {
+          title: '',
+          tag: [],
+          desc: '',
+          content: '',
+        },
+        rules: {
+          title: [
+            {
+              required: true,
+              message: '请输入博客标题',
+              trigger: 'blur',
+            },
+          ],
+          content: [
+            {
+              required: true,
+              message: '请输入博客内容',
+              trigger: 'blur',
+            },
+          ],
+        },
+      })
+      const submitForm = () => {
+        console.log(blogForm, blogForm.value)
+        blogForm.value.validate((valid) => {
+          if (valid) {
+            console.log(state.ruleForm)
+          } else {
+            console.log('error submit!!')
+            return false
+          }
+        })
+      }
+      const resetForm = () => {
+        blogForm.value.resetFields()
+      }
+      return {
+        ...toRefs(state),
+        blogForm,
+        submitForm,
+        resetForm,
+      }
+    },
+  })
 </script>
 
-<style scoped lang='scss'>
-  
+<style scoped lang="scss">
+  .blog-wrap {
+    margin: 40px;
+  }
 </style>
--- src/views/manageSystem/index.vue
+++ src/views/manageSystem/index.vue
@@ -1,44 +1,10 @@
 <template>
-  <el-container style="height: 800px; border: 1px solid #eee">
-  <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
-    <el-menu :default-openeds="['1']">
-      <el-submenu index="1">
-        <template #title><i class="el-icon-tickets"></i>博客</template>
-        <router-link index="1" to="/manageIndex/blog">
-          <el-menu-item index="1">博客管理</el-menu-item>
-        </router-link>
-        <!-- <el-menu-item index="1">博客管理</el-menu-item> -->
-        <el-menu-item-group>
-          <template #title>分组一</template>
-          <el-menu-item index="1-1">选项1</el-menu-item>
-          <el-menu-item index="1-2">选项2</el-menu-item>
-        </el-menu-item-group>
-        <el-submenu index="1-3">
-          <template #title>选项3</template>
-          <el-menu-item index="1-3-1">选项3-1</el-menu-item>
-        </el-submenu>
-      </el-submenu>
-      <el-submenu index="2">
-        <template #title><i class="el-icon-menu"></i>导航二</template>
-        <el-menu-item-group>
-          <template #title>分组一</template>
-          <el-menu-item index="2-1">选项1</el-menu-item>
-          <el-menu-item index="2-2">选项2</el-menu-item>
-        </el-menu-item-group>
-        <el-menu-item-group title="分组2">
-          <el-menu-item index="2-3">选项3</el-menu-item>
-        </el-menu-item-group>
-        <el-submenu index="2-4">
-          <template #title>选项4</template>
-          <el-menu-item index="2-4-1">选项4-1</el-menu-item>
-        </el-submenu>
-      </el-submenu>
-    </el-menu>
-  </el-aside>
   <el-container>
-    <el-header style="text-align: right; font-size: 12px">
+    <el-header style="display: flex;">
+      <p>博客管理系统</p>
+      <span>zhan-hc</span>
       <el-dropdown>
-        <i class="el-icon-setting" style="margin-right: 15px"></i>
+        <i class="el-icon-setting"></i>
         <template #dropdown>
           <el-dropdown-menu>
             <el-dropdown-item>个人中心</el-dropdown-item>
@@ -46,35 +12,79 @@
           </el-dropdown-menu>
         </template>
       </el-dropdown>
-      <span>zhan-hc</span>
     </el-header>
-    <el-main>
-      <router-view />
-    </el-main>
+    <el-container style="height: 800px; border: 1px solid #eee">
+      <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+        <el-menu :default-openeds="['1']">
+          <el-submenu index="1">
+            <template #title><i class="el-icon-tickets"></i>博客</template>
+            <router-link index="1" to="/manageIndex/blog">
+              <el-menu-item index="1">博客管理</el-menu-item>
+            </router-link>
+            <!-- <el-menu-item index="1">博客管理</el-menu-item> -->
+            <el-menu-item-group>
+              <template #title>分组一</template>
+              <el-menu-item index="1-1">选项1</el-menu-item>
+              <el-menu-item index="1-2">选项2</el-menu-item>
+            </el-menu-item-group>
+            <el-submenu index="1-3">
+              <template #title>选项3</template>
+              <el-menu-item index="1-3-1">选项3-1</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+          <el-submenu index="2">
+            <template #title><i class="el-icon-menu"></i>导航二</template>
+            <el-menu-item-group>
+              <template #title>分组一</template>
+              <el-menu-item index="2-1">选项1</el-menu-item>
+              <el-menu-item index="2-2">选项2</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="分组2">
+              <el-menu-item index="2-3">选项3</el-menu-item>
+            </el-menu-item-group>
+            <el-submenu index="2-4">
+              <template #title>选项4</template>
+              <el-menu-item index="2-4-1">选项4-1</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main>
+        <router-view />
+      </el-main>
+    </el-container>
   </el-container>
-</el-container>
 </template>
 
-<script lang='ts'>
-import { defineComponent } from 'vue'
+<script lang="ts">
+  import { defineComponent } from 'vue'
 
-export default defineComponent({
-  name: '',
-  props: {
-    
-  },
-  components: {
-  }
-})
+  export default defineComponent({
+    name: '',
+    props: {},
+    components: {},
+  })
 </script>
 
-<style scoped lang='scss'>
+<style scoped lang="scss">
   .el-header {
-    background-color: #B3C0D1;
+    background-color: #b3c0d1;
     color: #333;
-    line-height: 60px;
+    display: flex;
+    align-items: center;
+    p {
+      flex: 1;
+      font-weight: bold;
+      font-size: 20px;
+      letter-spacing: 1px;
+    }
+    span {
+      margin-right: 20px;
+    }
+    .el-icon-setting {
+      font-size: 18px;
+    }
   }
-  
   .el-aside {
     color: #333;
     height: 100%;
